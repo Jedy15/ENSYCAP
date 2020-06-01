2b5daaadc8e3c41b0066bfcb91d8c3d9058f1ea5
Ing Jorge Hdz <jedy15@hotmail.com>
2020-06-01 11:11:55 -0500

    primera configuracion de componentes con VUE. Ponentes

---
+++ resources/js/components/PonentesComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card" 
+                v-for="item in list" 
+                :key="item.id">
+                    <div class="card-header" v-text="item.ponente"></div>
+
+                    <div class="card-body">
+                        <p v-text="item.email"></p>
+                        <p v-text="item.usuario"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                list:[]
+            }
+        },
+        mounted(){
+            let url = 'http://ensycap.test/api/ponentes';
+            axios.get(url)
+            .then((response) => {
+                this.list = response.data                
+            });
+        }
+    }
+</script>
