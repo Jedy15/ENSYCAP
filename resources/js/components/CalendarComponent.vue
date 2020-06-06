2190303ea5063bc1007b86d2f795aa407f42d983
Ing Jorge Hdz <jedy15@hotmail.com>
2020-06-05 20:07:51 -0500

    Integracion de evento click e idioma al español de fullcalendar

--- resources/js/components/CalendarComponent.vue
+++ resources/js/components/CalendarComponent.vue
@@ -1,23 +1,29 @@
 <template>
+  <div>
     <FullCalendar 
         defaultView="dayGridMonth" 
         :plugins="calendarPlugins" 
         :weekends="calendarWeekends"
+        :selectable="selectable"
         :events="calendarEvents"
+        :locale="locales"
+        :weekNumbers= "weekNumbers"
         :header="{
-            left: 'prev,next today',
+            left: 'dayGridMonth,listMonth today',
             center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            right: 'prev,next'
         }"
+        @dateClick="handleDateClick"
     />
+  </div>
 </template>
 
 <script>
-import FullCalendar     from '@fullcalendar/vue'
-import dayGridPlugin    from '@fullcalendar/daygrid'
-import timeGridPlugin   from '@fullcalendar/timegrid'
-import interaction      from '@fullcalendar/interaction'
-import bootstrapPlugin  from '@fullcalendar/bootstrap'
+import FullCalendar       from '@fullcalendar/vue'
+import dayGridPlugin      from '@fullcalendar/daygrid'
+import interactionPlugin  from '@fullcalendar/interaction'
+import listPlugin         from '@fullcalendar/list'
+import esLocale           from '@fullcalendar/core/locales/es';
 
 
 export default {
@@ -26,11 +32,19 @@ components: {
   },
   data() {
     return {
-      calendarPlugins: [ interaction, dayGridPlugin, timeGridPlugin, bootstrapPlugin ],
+      calendarPlugins: [ interactionPlugin, dayGridPlugin, listPlugin ],
+      locales: esLocale,
       calendarWeekends: true,
+      selectable: true,
+      weekNumbers: true,
       calendarEvents: [ // initial event data
         { title: 'Event Now', start: new Date() }
       ]
+    }
+  },
+  methods: {
+    handleDateClick(info) {
+      alert('Clicked on: ' + info.dateStr);
     }
   }
 }
@@ -38,8 +52,7 @@ components: {
 
 <style lang='scss'>
 
-@import '~@fullcalendar/core/main.css';
-@import '~@fullcalendar/daygrid/main.css';
-@import '~@fullcalendar/bootstrap/main.css';
+  @import '~@fullcalendar/core/main.css';
+  @import '~@fullcalendar/daygrid/main.css';
 
 </style>
